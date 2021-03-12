fa1140546c68905277c62769441221387e81a07e
tranquangvinh <tranquangvinh5899@gmail.com>
2021-03-12 14:31:37 +0700

    add source

---
+++ index.js
@@ -0,0 +1,63 @@
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
+//import DeviceInfo which will help us to get UniqueId
+import DeviceInfo from 'react-native-device-info';
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+// Must be outside of any component LifeCycle (such as `componentDidMount`).
+PushNotification.configure({
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: function (token) {
+      var uniqueId = DeviceInfo.getUniqueId();
+      console.log("TOKEN:", token);
+      console.log("UNIQUE_ID:", uniqueId);
+    },
+  
+    // (required) Called when a remote is received or opened, or local notification is opened
+    onNotification: function (notification) {
+      console.log("NOTIFICATION:", notification);
+  
+      // process the notification
+  
+      // (required) Called when a remote is received or opened, or local notification is opened
+      notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+  
+    // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+    onAction: function (notification) {
+      console.log("ACTION:", notification.action);
+      console.log("NOTIFICATION:", notification);
+  
+      // process the action
+    },
+  
+    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+    onRegistrationError: function(err) {
+      console.error(err.message, err);
+    },
+  
+    // IOS ONLY (optional): default: all - Permissions to register.
+    permissions: {
+      alert: true,
+      badge: true,
+      sound: true,
+    },
+  
+    // Should the initial notification be popped automatically
+    // default: true
+    popInitialNotification: true,
+  
+    /**
+     * (optional) default: true
+     * - Specified if permissions (ios) and token (android and ios) will requested or not,
+     * - if not, you must call PushNotificationsHandler.requestPermissions() later
+     * - if you are not using remote notification or do not have Firebase installed, use this:
+     *     requestPermissions: Platform.OS === 'ios'
+     */
+    requestPermissions: true,
+  });
+registerRootComponent(App);
